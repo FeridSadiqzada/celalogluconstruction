a06e18b9d5745bcaaef9ebae6313b73b3bbc5276
FeridSadiqzada <feridsadiqzade@gmail.com>
2025-09-24 16:27:21 +0400

    Configure for static HTML depeürloydfsmdsedsndsatss

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: 'index.html'
-            }
-        }
-    }
 });
